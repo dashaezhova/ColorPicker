158f785a7e17905bca015ebdfd444dec2f16a275
andrey <andrey@davinci.davinci>
2014-10-22 11:27:34 +0400

    Added validation, and enter color in input

--- scripts/ColorPicker/utility/colorHelper.js
+++ scripts/ColorPicker/utility/colorHelper.js
@@ -2,8 +2,11 @@ define(['backbone'], function (Backbone) {
 	var colorHelper = (function () {
 	
 		// PRIVATE
+
+		var decHex;
+		var hexDec;
 		
-		var hexDec = function (hex) {
+		hexDec = function (hex) {
 			var m = hex.slice(1).match(/.{2}/g);
 			
 			m[0] = parseInt(m[0], 16);
@@ -11,6 +14,25 @@ define(['backbone'], function (Backbone) {
 			m[2] = parseInt(m[2], 16);
 		
 			return m;
+		};
+		
+		decHex = function (dec) {
+			var arr;
+			var i;
+
+			arr = [
+				(dec[0]).toString(16),
+				(dec[1]).toString(16),
+				(dec[2]).toString(16)
+			]
+			
+			for (i = 0; i < arr.length; i++) {
+				if (arr[i].length === 1) {
+					arr[i] = "0" + arr[i];
+				}			
+			}
+		
+			return arr;
 		};
 		
 		// PUBLIC
@@ -24,6 +46,10 @@ define(['backbone'], function (Backbone) {
 				var rgb;
 				var y;
 				
+				if (color.length === 4) {
+					color = "#" + color[1] + color[1] + color[2] + color[2] + color[3] + color[3];
+				}
+
 				rgb = hexDec(color);
 				
 				c = (255 - rgb[0]) / 255;
@@ -32,10 +58,10 @@ define(['backbone'], function (Backbone) {
 				k = Math.min(c, m, y);
 				
 				if (k !== 1) {
-					arr[0] = ((c - k)/(1 - k)).toFixed(2) * 100;
-					arr[1] = ((m - k)/(1 - k)).toFixed(2) * 100;
+					arr[0] = Math.round(((c - k)/(1 - k)).toFixed(2) * 100);
+					arr[1] = Math.round(((m - k)/(1 - k)).toFixed(2) * 100);
 					arr[2] = Math.round(((y - k)/(1 - k)).toFixed(2) * 100);
-					arr[3] = k.toFixed(2) * 100;
+					arr[3] = Math.round(k.toFixed(2) * 100);
 				}
 				
 				return colorObj = {
@@ -43,6 +69,24 @@ define(['backbone'], function (Backbone) {
 						rgb: rgb,
 						cmyk: arr
 					}
+			},
+			
+			conversionCmyk: function (color) {
+				var rgb;
+				var rgbHex;
+				
+				rgb = [
+					Math.round((100 - color[0]) * 2.55),
+					Math.round((100 - color[1]) * 2.55),
+					Math.round((100 - color[2]) * 2.55)
+				];
+
+				rgbHex = decHex(rgb);
+				
+				return colorObj = {
+					rgb: rgb,
+					rgbHex: rgbHex
+				}
 			}
 		};	
 	})();
--- scripts/ColorPicker/views/ColorPickerView.js
+++ scripts/ColorPicker/views/ColorPickerView.js
@@ -2,10 +2,12 @@ define([
 	'backbone',
 	'text!ColorPicker/templates/mainTemplate.html',
 	'text!ColorPicker/templates/inputTemplate.html',
-	'text!ColorPicker/templates/squareTemplate.html'
-], function (Backbone, MainTemplate, InputTemplate, SquareTemplate) {
+	'text!ColorPicker/templates/squareTemplate.html',
+	'ColorPicker/utility/colorHelper'
+], function (Backbone, MainTemplate, InputTemplate, SquareTemplate, colorHelper) {
 
 	var allTemplates = SquareTemplate + InputTemplate + MainTemplate;
+			
 	var ColorPickerView = Backbone.View.extend((function () {
 	
 		// PRIVATE
@@ -32,7 +34,10 @@ define([
 			
 			events: {
 				"change .radio-button": "makeActive",
-				"change #input-rgb": "validationRgb"
+				"focus #input-rgb": "validationRgb",
+				"focus #cmyk input[type=text]": "validationCmyk",
+				"click #submit": "setNewcolor",
+				"click #square": "openOrClosePicker"
 			},
 			
 			template: _.template(allTemplates),
@@ -49,7 +54,15 @@ define([
 				
 				setColorParams(that, rgb, cmyk);
 			},
-			
+			/*
+			asd: function () {
+				obj: {
+					odlCmyk:this.model.get("currentColorCmyk") 
+				}
+				return obj;
+				//	oldRgb = this.model.get("currentColorRgb");
+			},
+			*/
 			setValueModel: function () {
 				var cmyk = this.model.get('currentColorCmyk');
 				var rgb = this.model.get('currentColorRgb');
@@ -61,17 +74,151 @@ define([
 			setNewValueModel: function (cmyk, rgb) {
 				this.model.set("currentColorCmyk", cmyk);
 				this.model.set("currentColorRgb", rgb);
+				this.model.set("defaultColorSquare", rgb);
+				alert("добавилось");
 			},
 			
 			makeActive: function (event) {
 				this.$(".text-input").attr("disabled", "disabled");
 				$(event.currentTarget).parent().find(".text-input").removeAttr("disabled");
 			},
+			/*
+			openOrClosePicker: function () {
+				console.log($("#color-palette").css("display"));
+				console.log(this.asd.oldCmyk);
+
+			},
+			*/
+			testCorrectColorRgb: function (str) {
+				var colorObj;
+				var i;
+				var reg = /[A-Fa-f0-9]/;
+				var strLength;
+			
+				if (str.charAt(0) === '#') {
+					str = str.substring(1);
+				}
+
+				strLength = str.length;
+				
+				if (strLength === 3 || strLength === 6) {
+					for (i = 0; i < strLength; i++) {
+						if (!reg.test(str[i])) {
+							return false;
+						}
+					}
+					
+					return true;
+				}
+				
+				return false;			
+			},
+			
+			testCorrectColorCmyk: function (cmyk) {
+				var i;
+			
+				for (i = 0; i < cmyk.length; i++) {
+					if (isNaN(cmyk[i]) || cmyk[i] > 100 || cmyk[i].length < 1) {
+					
+						return false;
+					}
+				}
+				
+				return true;
+			},
+			
+			validationCmyk: function (event) {
+				var mainThis = this;
+				var that = this.$(event.currentTarget);
+				
+				return that.keydown(function() {
+					return setTimeout(function () {
+						var colorObj;
+						var rgbHex;
+						var valueInput;
+						
+						valueInput = [
+							this.$('#cmyk-input-c').val(),
+							this.$('#cmyk-input-m').val(),
+							this.$('#cmyk-input-y').val(),
+							this.$('#cmyk-input-k').val()
+						];
+						
+						if (!mainThis.testCorrectColorCmyk(valueInput)) {
+						
+							this.$("#square").css({"background-color" : mainThis.model.get("defaultColorSquare")});
+							this.$("#rgb input[type=text]").val(mainThis.model.get("currentColorRgb"));
+
+							return false;
+						}
+
+						colorObj = colorHelper.conversionCmyk(valueInput);
+						rgbHex = "#" + colorObj.rgbHex[0] + colorObj.rgbHex[1] + colorObj.rgbHex[2];
+						
+						if (valueInput[3] === "100") {
+							this.$("#rgb input[type=text]").val("#000000");
+							this.$("#square").css({"background-color" : "#000000"});
+						} else {
+							this.$("#rgb input[type=text]").val(rgbHex);
+							this.$("#square").css({"background-color" : rgbHex});
+						}		
+						
+					}, 10);
+				});
+			},
 			
 			validationRgb: function (event) {
-				var valueInput = $(event.currentTarget).val();
+				var mainThis = this;
+				var that = this.$(event.currentTarget);
 				
-				console.log(valueInput);
+				return that.keydown(function() {
+					return setTimeout(function () {
+						var colorObj;
+						var defaultCmyk = mainThis.model.get("currentColorCmyk");;
+						var valueInput;
+						
+						valueInput = that.val();
+					
+						if (!(valueInput.charAt(0) === '#')) {
+							valueInput = "#" + valueInput;
+						}
+
+						if (mainThis.testCorrectColorRgb(valueInput)) {
+							
+							colorObj = colorHelper.conversionRgb(valueInput);
+
+							setColorParams(this, colorObj.rgbHex, colorObj.cmyk);
+							this.$('#rgb input[type=text]').val(valueInput);
+
+							return true;
+						} else {
+							this.$("#square").css({"background-color" : mainThis.model.get("defaultColorSquare")});
+							
+							this.$("#cmyk-input-c").val(defaultCmyk[0]);
+							this.$("#cmyk-input-m").val(defaultCmyk[1]);
+							this.$("#cmyk-input-y").val(defaultCmyk[2]);
+							this.$("#cmyk-input-k").val(defaultCmyk[3]);
+						}
+						
+					}, 10);
+				});
+			},
+			
+			setNewcolor: function () {
+				// закрыть палетку
+				var cmyk;
+				var rgb = this.$("#rgb input[type=text]").val();
+				
+				cmyk = [
+					this.$('#cmyk-input-c').val(),
+					this.$('#cmyk-input-m').val(),
+					this.$('#cmyk-input-y').val(),
+					this.$('#cmyk-input-k').val()
+				];
+				
+				if (this.testCorrectColorCmyk(cmyk) && this.testCorrectColorRgb(rgb)) {
+					this.setNewValueModel(cmyk, rgb);
+				}
 			}
 		};
 	})());
